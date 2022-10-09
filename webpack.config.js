c3f36ae136212a7bd6a37554fdd0df96556fe76a
Whyukim <devhyukim@gmail.com>
2022-10-10 04:32:17 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const MyWebpackPlugin = require("./my-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "url-loader",
+        options: {
+          publicPath: "./dist/",
+          name: "[name].[ext]?[hash]",
+          limit: 20000, // 2kb
+        },
+      },
+    ],
+  },
+  plugins: [new MyWebpackPlugin()],
+};
