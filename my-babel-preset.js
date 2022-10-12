17fd6eced50b5788467fcd1c3439c70358ce5959
Whyukim <devhyukim@gmail.com>
2022-10-12 20:44:16 +0900

    update webpack+babel

---
+++ my-babel-preset.js
@@ -0,0 +1,9 @@
+module.exports = function MybabelPreset() {
+  return {
+    plugins: [
+      "@babel/plugin-transform-arrow-functions",
+      "@babel/plugin-transform-block-scoping",
+      "@babel/plugin-transform-strict-mode",
+    ],
+  };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
+const { captureRejectionSymbol } = require("events");
 const path = require("path");
-const MyWebpackPlugin = require("./my-webpack-plugin");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/app.js",
+    main: "./app.js",
   },
   output: {
     path: path.resolve("./dist"),
@@ -14,18 +19,58 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader
+            : "style-loader",
+          "css-loader",
+        ],
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: "url-loader",
         options: {
-          publicPath: "./dist/",
           name: "[name].[ext]?[hash]",
           limit: 20000, // 2kb
         },
       },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
     ],
   },
-  plugins: [new MyWebpackPlugin()],
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+      Build Date: ${new Date().toLocaleString()}
+      Commit Version: ${childProcess.execSync("git rev-parse --short HEAD")}
+      Commit Version: ${childProcess.execSync("git config user.name")}`,
+    }),
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify("v.1.2.3"),
+      PRODUCTION: JSON.stringify(false),
+      MAX_COUNT: JSON.stringify(999),
+      "api.domain": JSON.stringify("http://dev.api.domain.com"),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // 템플릿 경로를 지정
+      templateParameters: {
+        // 템플릿에 주입할 파라매터 변수 지정
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "",
+      },
+      // minify:
+      //   process.env.NODE_ENV === "production"
+      //     ? {
+      //         collapseWhitespace: true, // 빈칸 제거
+      //         removeComments: true, // 주석 제거
+      //       }
+      //     : false,
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production"
+      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+      : []),
+  ],
 };
